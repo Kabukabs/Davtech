8db9191fd1a243ba2bfd9b8f8448bd837e0d10d5
Elizabeth <lizbeepmc@gmail.com>
2024-07-21 21:47:16 +0300

    first page styled

--- src/components/CareerForms/Careerpage.jsx
+++ src/components/CareerForms/Careerpage.jsx
@@ -6,28 +6,28 @@ export const Careerpage = () => {
     <div className="relative h-screen flex flex-col">
     <div
       style={{ backgroundImage: `url(${careerimage})` }}
-      className="bg-cover bg-center h-[80vh] w-full bg-no-repeat"
+      className=" bg-cover bg-center h-[70vh] w-full bg-no-repeat"
     >
-      <div className="flex items-center justify-center h-full bg-black bg-opacity-40">
-      <div className="w-full max-w-md px-4 sm:px-6 lg:px-8 text-center text-white">
-        <h2 className="text-teal-500 text-5xl sm:text-4xl md:text-5xl lg:text-6xl mb-4">
+      <div className="flex items-center justify-center h-full bg-black bg-opacity-40 ">
+      <div className="w-full md:w-[70%] px-4 sm:px-6 lg:px-8 text-center text-white  my-[10rem]">
+        <h2 className="text-teal-500 text-[70px] sm:text-4xl md:text-5xl lg:text-6xl mb-4 font-extrabold">
           JOIN OUR TEAM
         </h2>
-        <p className="text-sm xsm:text-base md:text-lg lg:text-xl mb-4">
+        <p className="text-[17px]">
           DavTechinvest connects ambitious Professionals With Experienced
           Mentors And Advisors For Personalised Guidance And Career Growth.
           Skilled Individuals Can Gain Hands-On Experience Through Collaborative
           Projects.{" "}
         </p>
-        <h4 className="text-lg sm:text-xl md:text-2xl lg:text-3xl mb-4">JOIN AS A</h4>
-        <div className="mb-2">
+        <h4 className="text-[20px] text-teal-500 py-8 px-4  ">JOIN AS A</h4>
+        <div className="mb-6">
         <Link to="/skill-collab">
-            <button className="bg-teal-500 text-blue py-2 px-4 rounded text-sm sm:text-base md:text-lg lg:text-xl">SKILLFUL COLLABORATION</button>
+            <button className="bg-white text-blue py-2 px-6 rounded-[30px] font-extrabold w-full md:w-[300px] h-12">SKILLFUL COLLABORATION</button>
           </Link>
         </div>
         <div>
         <Link to="/mentor-advisor">
-            <button className="bg-teal-500 text-blue py-2 px-4 rounded text-sm sm:text-base md:text-lg lg:text-xl">MENTOR/ADVISOR</button>
+            <button className="bg-white text-blue py-2 px-6 rounded-[30px] font-extrabold w-full md:w-[300px] h-12">MENTOR/ADVISOR</button>
           </Link>
         </div>
       </div>
--- src/components/CareerForms/form2.jsx
+++ src/components/CareerForms/form2.jsx
@@ -5,7 +5,7 @@ const initialState = {
   email: "",
   phone: "",
   experience: "",
-  cv: "",
+  cv: null,
   joinas1: false,
   joinas2: false,
 };
@@ -25,64 +25,86 @@ export default function MentorAdvisor() {
       setForm({ ...form, [name]: value });
     }
   };
+  const handleUpload = (event) => {
+    const { name, files } = event.target;
+    setForm({ ...form, [name]: files[0] });
+  };
   return (
-    <form onSubmit={{ submitForm }}>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+    <form onSubmit={{ submitForm }} enctype= "multipart/form-data" className="w-full max-w-lg   space-y-6">
       <div>
-        <h2>MENTOR/ADVISOR</h2>
-        <p>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">MENTOR/ADVISOR</h2>
+        <p className="text-gray-600 mb-4">
           Join DavTechinvest As A Mentor Or Advisor To Shape The Future Of Technology And Careers By Sharing Your Expertise With Aspiring Tech Professionals. You`ll Empower The next Generation Of Leaders, Expand Your Network , Stay Updated On Industry Trends, And Play A Pivotal Role In Transforming Ideas Into Successful Ventures. Fill Out The Form Below To Express Your Interest And Help Cultivate
           Talent And Drive Innovation.
         </p>
-        <p>
+        <p className="text-gray-600 mb-4">
           fill out the form below to express your interest and help cultivate
           talent and drive innovation.
         </p>
       </div>
-      <div>
-        <label>
-          *Name
+      <div className="space-y-4">
+        <label className="block text-gray-700">
+        <span className="text-red-500">*</span>
+          Name
           <input
             onChange={(event) => handleChange(event)}
             type="text"
             name="name"
             value={form.name}
             required
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:border-teal-500 focus:ring focus:ring-teal-200 focus:ring-opacity-50"
           />
         </label>
-        <label>
-          *Email
+        <label className="block text-gray-700">
+        <span className="text-red-500">*</span>
+          Email
           <input
             onChange={(event) => handleChange(event)}
             type="email"
             name="email"
             value={form.email}
             required
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:border-teal-500 focus:ring focus:ring-teal-200 focus:ring-opacity-50"
           />
         </label>
-        <label>
-          *Phone
+        <label className="block text-gray-700">
+        <span className="text-red-500">*</span>
+          Phone
           <input
             onChange={(event) => handleChange(event)}
             type="number"
             name="phone"
             value={form.phone}
             required
+             className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:border-teal-500 focus:ring focus:ring-teal-200 focus:ring-opacity-50"
           />
         </label>
-        <label>
-          *Experience
+        <label  className="block text-gray-700">
+        <span className="text-red-500">*</span>
+          Experience
           <textarea
             onChange={(event) => handleChange(event)}
             name="experience"
             value={form.experience}
             required
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:border-teal-500 focus:ring focus:ring-teal-200 focus:ring-opacity-50"
           />
         </label>
-        <label>CV Upload</label>
+        <label>CV Upload
+        <input
+            onChange={handleUpload}
+            type="file"
+            name="cv"
+            className="mt-1 block w-full text-gray-700 border border-gray-300 rounded-lg shadow-sm"
+
+          />
+        </label>
       </div>
-      <div>
-         <h3>Join As</h3>
-        <label>Mentor
+      <div className="block text-gray-700">
+         <h3 className="text-lg font-semibold mb-2">Join As</h3>
+         <div className="flex items-center mb-2">
+        <label className="text-gray-700">Mentor
         <input
                onChange= {(event) =>
                   handleChange(event)
@@ -90,21 +112,28 @@ export default function MentorAdvisor() {
                checked = {form.mentor}
                type = "checkbox"
                name = "joinas1"
-               value= "Mentor"/>
+               value= "mentor"
+               />
         </label>
-        <label>Advisor
+        /</div>
+        <div >
+        <label >Advisor
         <input
                onChange= {(event) =>
                   handleChange(event)
                }
-               checked = {form.mentor}
+               checked = {form.advisor}
                type = "checkbox"
                name = "joinas2"
-               value= "Advisor"/>
+               value= "advisor"
+               />
         </label>
+        </div>
 
       </div>
-      <input type="submit" value="Submit" />
+      <input type="submit" value="Submit" 
+      className="w-full bg-teal-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"/>
     </form>
+    </div>
   );
 }
