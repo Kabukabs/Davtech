767b9bdad40068f0f6c32bb3a42f163038c94ced
Elizabeth <lizbeepmc@gmail.com>
2024-07-24 14:20:30 +0300

    thank you page styled

--- src/components/CareerForms/Thankyou.jsx
+++ src/components/CareerForms/Thankyou.jsx
@@ -3,33 +3,38 @@ import careerimage from "/careerimage.svg";
 export default function Thankyou() {
   const telegramNumber = +111111111111;
   return (
-    <div
+    <div className="relative h-screen flex flex-col">
+    <div 
       style={{ backgroundImage: `url(${careerimage})` }}
-      className="bg-cover h-screen flex items-center justify-center"
+      class="bg-cover bg-center  w-full p-24 h-[70vh] "
     >
-      <div className="w-full max-w-md ">
-        <h2 className="text-teal-500 font-size: 8rem; text-5xl">
+      <div class=" w-full max-w-[800px] flex-row justify-center items-center">
+      
+      <h2 className="text-teal-500 text-[70px] sm:text-4xl md:text-5xl lg:text-6xl mb-4 font-extrabold">
           JOIN OUR TEAM
         </h2>
-        <p>
+        <p className="text-white text-lg ">
           DavTechinvest connects ambitious Professionals With Experienced
           Mentors And Advisors For Personalised Guidance And Career Growth.
           Skilled Individuals Can Gain Hands-On ExperienceThrough Collaborative
           Projects.{" "}
         </p>
-      </div>
-      <div>
-        <img src="/Thankyou.jpg" alt="Thank You" />
-        <p>Thank You For Joining!</p>
-        <a
+        </div>
+        <img src="/Thankyou.jpg" alt="Thank You" 
+         className="w-64 h-auto object-cover rounded-lg shadow-lg"/>
+         <p className="">Thank You For Joining!</p>
+         <a
           href={`https://wa.me/${telegramNumber}`}
           target="_blank"
           rel="noopener noreferrer"
+           className="text-teal-500 underline"
         >
           Click Here To Connect With Us On Telegram And Stay Updated On All Our
           Latest News And Opportunities
         </a>
-      </div>
+    
     </div>
+    </div>
+   
   );
 }
--- src/components/CareerForms/form1.jsx
+++ src/components/CareerForms/form1.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 const initialState = {
   name: "",
   email: "",
@@ -14,7 +14,7 @@ export default function SkillCollab() {
   const submitForm = (event) => {
     event.preventDefault();
     setForm(initialState);
-    navigate('/thank-you');
+    navigate("/thank-you");
   };
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -28,125 +28,142 @@ export default function SkillCollab() {
 
   return (
     <div className="  flex items-center justify-center min-h-screen w-full  p-4 bg-babyblue">
-    <form onSubmit={ submitForm }  enctype= "multipart/form-data" class="w-full max-w-[800px]">
-      <div >
-        <h2 className=" font-bold text-darkblue mb-6 text-[40px]">SKILLFUL COLLABORATION</h2>
-        <p className="text-darkblue text-[20px] mb-6">
-          Embark On A Transformative Journey With DavTechinvest, Where You Can
-          Access Personalized Mentorship, Engage In Collaborative Projects, And Build A Standout Portfolio. Turn Your Ideas Into Reality And Gain Hands-On Experience While Potentially Becoming A Startup Owner. Ready To Elevate Your Career? Join Our Dynamic Community Today!
-        </p>
-        <p className=" text-darkblue text-[20px] mb-5">
-          fill out the form below to express your interest and help cultivate
-          talent and drive innovation.
-        </p>
-      </div>
-      <div class="md:flex md:items-center mb-6">
-        
-        <label class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] " for="inline-name">
-        <span className="text-red-500">*</span>
-          Name:
-          </label>
-          
-          <div class="w-full overflow-hidden">
-          <input
-            onChange={(event) => handleChange(event)}
-            type="text"
-            name="name"
-            value={form.name}
-            required
-            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500" 
-          />
-          </div>
-          </div>
-          <div class="md:flex md:items-center mb-6">
-          
-        <label class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] " for="inline-email">
-        <span className="text-red-500">*</span>
-          Email:
-          </label>
-          
-          <div class="w-full overflow-hidden">
-          <input
-            onChange={(event) => handleChange(event)}
-            type="email"
-            name="email"
-            value={form.email}
-            required
-            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-16 focus:outline-none focus:bg-white focus:border-purple-500" 
-          />
-          </div>
-          </div>
-          <div class="md:flex md:items-center mb-10">
-          
-        <label class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] " for="inline-phone">
-        <span className="text-red-500">*</span>
-          Phone:
-          </label>
-          
-          <div class="w-full overflow-hidden">
-          <input
-            onChange={(event) => handleChange(event)}
-            type="number"
-            name="phone"
-            value={form.phone}
-            required
-            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500" 
-          /> 
-          </div>
-          </div>
-          <div class="md:flex md:items-center mb-6">
-          
-        <label class="block text-darkblue  md:text-right  mb-2 text-[15px] " for="inline-experience">
-        <span className="text-red-500">*</span>
-          Experience:
-          </label>
-         
-          <div class="w-full overflow-hidden">
-          <textarea
-            onChange={(event) => handleChange(event)}
-            name="experience"
-            value={form.experience}
-            required
-            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-6 mt-10 focus:outline-none focus:bg-white focus:border-purple-500"  id="inline-experience"
-            rows="5" cols="50"
-          />
-          </div>
-       </div>
-       <div class="md:flex md:items-center mb-6">
-       
-       <label class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] " for="inline-cv">
-          CV Upload
-          <input
-            onChange={handleUpload}
-            type="file"
-            name="cv"
-            className="hidden"
-            id="cvUpload"
-          />
-          <label
-            htmlFor="cvUpload"
-            className="bg-gray-200 border-2 border-gray-200  w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ml-10 cursor-pointer"
-          >
-            {cvFileName}
-          </label>
-        </label>
-       
+      <form
+        onSubmit={submitForm}
+        enctype="multipart/form-data"
+        class="w-full max-w-[800px]"
+      >
+        <div>
+          <h2 className=" font-bold text-darkblue mb-6 text-[40px]">
+            SKILLFUL COLLABORATION
+          </h2>
+          <p className="text-darkblue text-[20px] mb-6">
+            Embark On A Transformative Journey With DavTechinvest, Where You Can
+            Access Personalized Mentorship, Engage In Collaborative Projects,
+            And Build A Standout Portfolio. Turn Your Ideas Into Reality And
+            Gain Hands-On Experience While Potentially Becoming A Startup Owner.
+            Ready To Elevate Your Career? Join Our Dynamic Community Today!
+          </p>
+          <p className=" text-darkblue text-[20px] mb-5">
+            fill out the form below to express your interest and help cultivate
+            talent and drive innovation.
+          </p>
         </div>
-       
-        
-     
+        <div class="md:flex md:items-center mb-6">
+          <label
+            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
+            for="inline-name"
+          >
+            <span className="text-red-500">*</span>
+            Name:
+          </label>
+
+          <div class="w-full overflow-hidden">
+            <input
+              onChange={(event) => handleChange(event)}
+              type="text"
+              name="name"
+              value={form.name}
+              required
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <label
+            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
+            for="inline-email"
+          >
+            <span className="text-red-500">*</span>
+            Email:
+          </label>
+
+          <div class="w-full overflow-hidden">
+            <input
+              onChange={(event) => handleChange(event)}
+              type="email"
+              name="email"
+              value={form.email}
+              required
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-16 focus:outline-none focus:bg-white focus:border-purple-500"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-10">
+          <label
+            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
+            for="inline-phone"
+          >
+            <span className="text-red-500">*</span>
+            Phone:
+          </label>
+
+          <div class="w-full overflow-hidden">
+            <input
+              onChange={(event) => handleChange(event)}
+              type="number"
+              name="phone"
+              value={form.phone}
+              required
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <label
+            class="block text-darkblue  md:text-right  mb-2 text-[15px] "
+            for="inline-experience"
+          >
+            <span className="text-red-500">*</span>
+            Experience:
+          </label>
+
+          <div class="w-full overflow-hidden">
+            <textarea
+              onChange={(event) => handleChange(event)}
+              name="experience"
+              value={form.experience}
+              required
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-6 mt-10 focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-experience"
+              rows="5"
+              cols="50"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <label
+            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
+            for="inline-cv"
+          >
+            CV Upload
+            <input
+              onChange={handleUpload}
+              type="file"
+              name="cv"
+              className="hidden"
+              id="cvUpload"
+            />
+            <label
+              htmlFor="cvUpload"
+              className="bg-gray-200 border-2 border-gray-200  w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ml-10 cursor-pointer"
+            >
+              {cvFileName}
+            </label>
+          </label>
+        </div>
+
         <div class="md:flex md:items-center">
-        <div class="md:w-1/3"></div>
-        <div class="md:w-2/3">
-      <input type="submit" value="Submit" 
-      class="shadow bg-blue  mt-4 text-white font-bold py-2 px-4 rounded"/>
-      </div>
-      </div>
-    </form>
+          <div class="md:w-1/3"></div>
+          <div class="md:w-2/3">
+            <input
+              type="submit"
+              value="Submit"
+              class="shadow bg-blue  mt-4 text-white font-bold py-2 px-4 rounded"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
-
-
-
-  
