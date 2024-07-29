775ec3fbca1264806ef12df94a72aa310890b1a9
Sharon Lawal <sharonayolawal@gmail.com>
2024-07-29 13:34:52 +0100

    Added firebase

--- src/components/CareerForms/form1.jsx
+++ src/components/CareerForms/form1.jsx
@@ -1,140 +1,169 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { db, storage } from '../CareerForms/firebaseConfig';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { collection, addDoc } from 'firebase/firestore';
+
 const initialState = {
   name: '',
   email: '',
   phone: '',
   experience: '',
   cv: null,
-}
+};
+
 export default function SkillCollab() {
   const [form, setForm] = useState(initialState);
-  const navigate = useNavigate();
   const [cvFileName, setCvFileName] = useState('Click To Upload');
-  const submitForm = (event) => {
-    event.preventDefault();
-    setForm(initialState);
-    navigate('/thank-you');
-  };
+  const navigate = useNavigate();
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setForm({ ...form, [name]: value });
   };
+
   const handleUpload = (event) => {
     const { name, files } = event.target;
     setForm({ ...form, [name]: files[0] });
     setCvFileName(files[0] ? files[0].name : 'Click To Upload');
   };
 
+  const submitForm = async (event) => {
+    event.preventDefault();
+
+    try {
+      let cvUrl = '';
+      if (form.cv) {
+        const cvRef = ref(storage, `cv/${form.cv.name}`);
+        await uploadBytes(cvRef, form.cv);
+        cvUrl = await getDownloadURL(cvRef);
+      }
+
+      await addDoc(collection(db, 'skill_collaborators'), {
+        name: form.name,
+        email: form.email,
+        phone: form.phone,
+        experience: form.experience,
+        cv: cvUrl,
+      });
+
+      setForm(initialState);
+      navigate('/thank-you');
+    } catch (error) {
+      console.error('Error submitting form: ', error);
+      alert('There was an error submitting the form. Please try again.');
+    }
+  };
+
   return (
-    <div className="  flex items-center justify-center min-h-screen w-full  p-4 bg-babyblue">
+    <div className="flex items-center justify-center min-h-screen w-full p-4 bg-babyblue">
       <form
         onSubmit={submitForm}
-        enctype="multipart/form-data"
-        class="w-full max-w-[800px]"
+        encType="multipart/form-data"
+        className="w-full max-w-[800px]"
       >
         <div>
-          <h2 className=" font-bold text-darkblue mb-6 text-[40px]">
+          <h2 className="font-bold text-darkblue mb-6 text-[40px]">
             SKILLFUL COLLABORATION
           </h2>
           <p className="text-darkblue text-[20px] mb-6">
-            Embark On A Transformative Journey With DavTechinvest, Where You Can
-            Access Personalized Mentorship, Engage In Collaborative Projects,
-            And Build A Standout Portfolio. Turn Your Ideas Into Reality And
-            Gain Hands-On Experience While Potentially Becoming A Startup Owner.
-            Ready To Elevate Your Career? Join Our Dynamic Community Today!
+            Embark on a transformative journey with DavTechinvest, where you can
+            access personalized mentorship, engage in collaborative projects,
+            and build a standout portfolio. Turn your ideas into reality and
+            gain hands-on experience while potentially becoming a startup owner.
+            Ready to elevate your career? Join our dynamic community today!
           </p>
-          <p className=" text-darkblue text-[20px] mb-5">
-            fill out the form below to express your interest and help cultivate
+          <p className="text-darkblue text-[20px] mb-5">
+            Fill out the form below to express your interest and help cultivate
             talent and drive innovation.
           </p>
         </div>
-        <div class="md:flex md:items-center mb-6">
+        <div className="md:flex md:items-center mb-6">
           <label
-            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
-            for="inline-name"
+            className="block text-darkblue md:text-right mb-1 md:mb-0 text-[15px]"
+            htmlFor="inline-name"
           >
             <span className="text-red-500">*</span>
             Name:
           </label>
 
-          <div class="w-full overflow-hidden">
+          <div className="w-full overflow-hidden">
             <input
-              onChange={(event) => handleChange(event)}
+              onChange={handleChange}
               type="text"
               name="name"
               value={form.name}
               required
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
             />
           </div>
         </div>
-        <div class="md:flex md:items-center mb-6">
+        <div className="md:flex md:items-center mb-6">
           <label
-            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
-            for="inline-email"
+            className="block text-darkblue md:text-right mb-1 md:mb-0 text-[15px]"
+            htmlFor="inline-email"
           >
             <span className="text-red-500">*</span>
             Email:
           </label>
 
-          <div class="w-full overflow-hidden">
+          <div className="w-full overflow-hidden">
             <input
-              onChange={(event) => handleChange(event)}
+              onChange={handleChange}
               type="email"
               name="email"
               value={form.email}
               required
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-16 focus:outline-none focus:bg-white focus:border-purple-500"
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-16 focus:outline-none focus:bg-white focus:border-purple-500"
             />
           </div>
         </div>
-        <div class="md:flex md:items-center mb-10">
+        <div className="md:flex md:items-center mb-10">
           <label
-            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
-            for="inline-phone"
+            className="block text-darkblue md:text-right mb-1 md:mb-0 text-[15px]"
+            htmlFor="inline-phone"
           >
             <span className="text-red-500">*</span>
             Phone:
           </label>
 
-          <div class="w-full overflow-hidden">
+          <div className="w-full overflow-hidden">
             <input
-              onChange={(event) => handleChange(event)}
+              onChange={handleChange}
               type="number"
               name="phone"
               value={form.phone}
               required
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
             />
           </div>
         </div>
-        <div class="md:flex md:items-center mb-6">
+        <div className="md:flex md:items-center mb-6">
           <label
-            class="block text-darkblue  md:text-right  mb-2 text-[15px] "
-            for="inline-experience"
+            className="block text-darkblue md:text-right mb-2 text-[15px]"
+            htmlFor="inline-experience"
           >
             <span className="text-red-500">*</span>
             Experience:
           </label>
 
-          <div class="w-full overflow-hidden">
+          <div className="w-full overflow-hidden">
             <textarea
-              onChange={(event) => handleChange(event)}
+              onChange={handleChange}
               name="experience"
               value={form.experience}
               required
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-6 mt-10 focus:outline-none focus:bg-white focus:border-purple-500"
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-6 mt-10 focus:outline-none focus:bg-white focus:border-purple-500"
               id="inline-experience"
               rows="5"
               cols="50"
             />
           </div>
         </div>
-        <div class="md:flex md:items-center mb-6">
+        <div className="md:flex md:items-center mb-6">
           <label
-            class="block text-darkblue  md:text-right mb-1 md:mb-0 text-[15px] "
-            for="inline-cv"
+            className="block text-darkblue md:text-right mb-1 md:mb-0 text-[15px]"
+            htmlFor="inline-cv"
           >
             CV Upload
             <input
@@ -146,20 +175,20 @@ export default function SkillCollab() {
             />
             <label
               htmlFor="cvUpload"
-              className="bg-gray-200 border-2 border-gray-200  w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ml-10 cursor-pointer"
+              className="bg-gray-200 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ml-10 cursor-pointer"
             >
               {cvFileName}
             </label>
           </label>
         </div>
 
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3"></div>
-          <div class="md:w-2/3">
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
             <input
               type="submit"
               value="Submit"
-              class="shadow bg-blue  mt-4 text-white font-bold py-2 px-4 rounded"
+              className="shadow bg-blue mt-4 text-white font-bold py-2 px-4 rounded cursor-pointer"
             />
           </div>
         </div>
