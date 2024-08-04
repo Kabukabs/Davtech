54fd6c6369fd7229302a13ca4567fa620bb22e18
Elizabeth <lizbeepmc@gmail.com>
2024-08-04 14:59:08 +0300

    merged upstream and added new styling

--- src/components/ContactUSForm/contactform.jsx
+++ src/components/ContactUSForm/contactform.jsx
@@ -6,6 +6,7 @@ const initialState = {
   email: "",
   message: "",
 };
+const recaptchaKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY
 export default function ContactUsForm() {
   const [form, setForm] = useState(initialState);
   const [captchaValue, setCaptchaValue] = useState(null);
@@ -31,9 +32,7 @@ export default function ContactUsForm() {
     }
 
     emailjs
-      .sendForm('service_cjx0wxe', 'template_evb8bo7', formRef.current, {
-        publicKey: 'DPzMJ9yxiCT6adfIN',
-      })
+      .sendForm('service_cjx0wxe', 'template_evb8bo7', formRef.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
       .then((result) => {
           setSuccess(true);
           setError(false);
@@ -49,7 +48,7 @@ export default function ContactUsForm() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <form onSubmit ={sendEmail} ref={formRef}className="w-full max-w-[600px]">
+      <form onSubmit ={sendEmail} ref={formRef}className="w-full max-w-[600px] mt-10">
        <div className="mb-4">
         <input 
         onChange={(event) => handleChange(event)}
@@ -58,7 +57,7 @@ export default function ContactUsForm() {
         name="name"
         required
         placeholder="Name"
-        class=" appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"
+        class=" appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-black-500"
         />
         </div> 
         <div className="mb-6 mt-10">
@@ -69,34 +68,34 @@ export default function ContactUsForm() {
             placeholder="Email Address"
             value = {form.email}
             required
-            class=" appearance-none border-2 border-black-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-purple-500"/>
+            class=" appearance-none border-2 border-black-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-14 focus:outline-none focus:bg-white focus:border-black-500"/>
         </div>
         <div className="mb-4 mt-10 ml-10">
-            <p className="text-[15px]"> <strong>Please enter the email address where you wish to receive our answer. If you are a registered member of DavTechinvest, please include the email address you used when you registered if possible to help us locate your account as soon as possible. </strong></p>
+            <p className="text-[15px] ml-4"> <strong>Please enter the email address where you wish to receive our answer. If you are a registered member of DavTechinvest, please include the email address you used when you registered if possible to help us locate your account as soon as possible. </strong></p>
         </div>
-        <div>
+        <div className="ml-8">
             <textarea
             onChange={(event) => handleChange(event)}
             name = "message"
             placeholder="Message"
             value = {form.message}
-            class=" mb-4 ml-10 appearance-none border-2 border-black-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-6 mt-10 focus:outline-none focus:bg-white focus:border-purple-500"
+            className="   appearance-none border-2 border-black-200 rounded w-full py-2 px-4 text-gray-700 leading-tight ml-6 mt-10 focus:outline-none focus:bg-white focus:border-black-500"
               id="inline-experience"
-              rows="5"
+              rows="8"
               cols="50"
               required/>
         </div>
-        <div className="mb-4 ml-10 flex">
-          <ReCAPTCHA
-            sitekey="6LftdhwqAAAAADcfG8owy9QpSc-B6yoKmbmLpLeA" // Replace with your reCAPTCHA site key
+        <div className="mb-4 ml-14 flex">
+          <ReCAPTCHA className="mt-10"
+            sitekey= {recaptchaKey}// Replace with your reCAPTCHA site key
             onChange={handleCaptchaChange}
           />
         </div>
-        <div className="mb-4 text-center">
+        <div className="mb-4 flex justify-end">
             <input
             type="submit"
             value="submit"
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"></input>
+            className="bg-purple-500 hover:bg-purple-700 text-white  py-2 px-4 "></input>
             {error && <p className="text-red-500 mt-2">Error occured while sending the message. Please try again</p>}
             {success && <p className="text-green-500 mt-2">Message sent successfully!</p>}
         </div>
