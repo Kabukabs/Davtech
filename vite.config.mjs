a12316b070793c1f8e23a3ebce8b990c322b7dfc
Sharon Lawal <sharonayolawal@gmail.com>
2025-01-07 04:55:25 +0100

    made changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import dotenv from 'dotenv';
 
+// Load environment variables from a `.env` file
 dotenv.config({ path: path.resolve(__dirname, './sample.env') });
 
 // https://vitejs.dev/config/
@@ -10,7 +11,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'), // Alias for easier imports
     },
   },
-})
+  build: {
+    outDir: 'build', // Specify the output directory
+    sourcemap: true, // Generate sourcemaps (set to false if you want to disable them)
+  },
+});
